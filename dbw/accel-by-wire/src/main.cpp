5dc61deb6f9acc1e55cb7bd292fa8f7d6bfc0725
daniel.tsarev@cooper.edu <daniel.tsarev@cooper.edu>
2021-10-13 14:19:01 -0400

    DBW-9: fix board in platformio.ini and pretty up serial output

--- dbw/accel-by-wire/src/main.cpp
+++ dbw/accel-by-wire/src/main.cpp
@@ -34,8 +34,17 @@ struct PWM pwm2 = {
 void pwm_percent(struct PWM pwm, float vdd, float percent) {
   float voltage = (percent/100.0) * (pwm.r2 - pwm.r1) + (pwm.r1 + pwm.offset); 
   int pwm_level = voltage * (256.0/vdd);
+
+  Serial.print("Accel throttle: "); 
+  Serial.print(percent);
+  Serial.println("%");
+
+  Serial.print("\tDesired voltage: "); 
+  Serial.print(voltage - pwm.offset);
+  Serial.print("V || Output voltage: "); 
   Serial.print(voltage);
   Serial.println("V");
+
   ledcWrite(pwm.ledc_channel, pwm_level);
 }
 
@@ -56,5 +65,5 @@ void loop() {
   Serial.print("PWM2: ");
   pwm_percent(pwm2, 5.0, 0.0);
 
-  delay(500);
+  delay(1000);
 }
