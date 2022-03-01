a5ae351a268b3ae9b620bf6701f78e7b0e48753d
Dan Mezhiborsky <daniel.mezhiborsky@cooper.edu>
2022-02-28 22:30:27 -0500

    DBW-31 Initial bootloader

--- dbw/node_fw/igvc_bl/src/can.c
+++ dbw/node_fw/igvc_bl/src/can.c
@@ -1,13 +1,7 @@
 #include "can.h"
 
-#include <esp_ota_ops.h>
 #include <driver/twai.h>
 #include <stdio.h>
-
-#include "base/base.h"
-#include "common.h"
-#include "sys/task_glue.h"
-#include "sys/watchdog.h"
 
 // ######        DEFINES        ###### //
 
@@ -16,21 +10,13 @@
 
 // ######      PROTOTYPES       ###### //
 
-static void can_init();
-static void can_10Hz_ping();
-
 // ######     PRIVATE DATA      ###### //
 
 static bool can_ready;
 
 // ######    RATE FUNCTIONS     ###### //
 
-struct rate_funcs can_rf = {
-    .call_init = can_init,
-    .call_10Hz = can_10Hz_ping,
-};
-
-static void can_init()
+void can_init()
 {
     twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(CAN_TX_GPIO, CAN_RX_GPIO, TWAI_MODE_NORMAL);
     twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
@@ -49,19 +35,19 @@ static void can_init()
     }
 }
 
-static void can_10Hz_ping()
+void can_ping()
 {
     static uint8_t count = 0;
 
     if (!can_ready) {
-        base_set_state_lost_can();
+        // base_set_state_lost_can();
         return;
     }
 
     twai_message_t message;
     message.extd = 0;
 
-    message.identifier = 0x1;
+    message.identifier = 0x9;
 
     message.data_length_code = 2;
 
@@ -72,34 +58,12 @@ static void can_10Hz_ping()
 
     esp_err_t r = twai_transmit(&message, 0);
 
-    if (r == ESP_OK) {
-        base_set_state_good();
-    } else {
-        base_set_state_lost_can();
-        // attempt recovery?
-    }
-
-    if (count == 0x50) {
-        const esp_partition_t* factory = esp_partition_find_first(
-            ESP_PARTITION_TYPE_APP,
-            ESP_PARTITION_SUBTYPE_APP_FACTORY,
-            NULL
-        );
-        if (!factory) {
-            count = 0x99;
-        } else {
-            set_up_rtc_watchdog_fwupdate(); // give us some time
-            esp_err_t err = esp_ota_set_boot_partition(factory);
-            if (err == ESP_OK)
-                esp_restart();
-
-            count = err;
-        }
-    }
-
-    if (count == 0x51) {
-        esp_restart();
-    }
+    // if (r == ESP_OK) {
+    //     base_set_state_good();
+    // } else {
+    //     base_set_state_lost_can();
+    //     // attempt recovery?
+    // }
 
 }
 
